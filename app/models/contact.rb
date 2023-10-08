b7dd0d77bd8ca9e0c6a2b89b70769a39f2e7a1ea
bpereira74 <bpereira74@hotmail.com>
2023-10-08 18:28:18 -0300

    feat: gemas annotate y faker

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: contacts
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  email      :string
+#  message    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Contact < ApplicationRecord
 end
